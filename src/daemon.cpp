3dcf87b87df84ec6dbde7a7b3bab98f57a190ac6
Luca Bertoni <luca.bertoni24@gmail.com>
2015-07-27 16:33:49 +0200

    Update install.sh

--- src/daemon.cpp
+++ src/daemon.cpp
@@ -2,3 +2,6 @@
 	Demone che gestisce i messaggi in coda
 */
 
+int main(){
+	return 0;
+}
--- src/install.sh
+++ src/install.sh
@@ -1,7 +1,9 @@
 clear
 
-echo "Sto compilando..."
-
+echo "Sto compilando il main..."
 g++ main.cpp class/*.cpp -o main.o -std=c++0x -lcurl -lmysqlcppconn
+echo "Compilato!"
 
+echo "Sto compilando il demone..."
+g++ daemon.cpp class/*.cpp -o daemon.o -std=c++0x -lcurl -lmysqlcppconn
 echo "Compilato!"
